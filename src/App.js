e0c433347498ee59f4d11f64ecc152a5c57b3031
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-17 15:59:15 -0500

    everything working except hot reload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, { Component }  from 'react'
 export default class App extends Component {
   render() {
     return(
-      <div className='body' />
+      <div className='body'>
+      <h1>
+        Hello World!
+      </h1>
+      </div>
     )
   }
 }
