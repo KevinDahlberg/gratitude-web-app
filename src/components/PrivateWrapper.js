0a2fd2d50563e280ac89a1e8f67d883467f77a0e
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-11-03 20:42:46 -0500

    saving changes

---
+++ src/components/PrivateWrapper.js
@@ -0,0 +1,11 @@
+
+const PrivateWrapper = ({component: Component, authed, ...rest}) => {
+    return (
+        <Route
+            {...rest}
+            render={(props) => authed === true 
+                ? <Component {...props} /> 
+                : <Redirect to={{pathname: './login', state: {from: props.location}}} />}
+        />
+    )
+}
